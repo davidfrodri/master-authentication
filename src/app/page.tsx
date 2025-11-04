4dfcf37e675c2c1ce6b7c94161e4be4b9ae87637
David Rodriguez <davidfrodricomputerscience@gmail.com>
2025-11-04 18:49:06 -0500

    feat: button added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
    <div className="my-6 px-4 max-w-md mx-auto">
     <h1 className="text-3xl font-bold">Welcome to auth app</h1>
-    <Link href="/auth/login">Sign In / Sign Up</Link>
+    <Button asChild size="lg">
+      <Link href="/auth/login">Sign In / Sign Up</Link>
+    </Button>
    </div>
   );
 }
